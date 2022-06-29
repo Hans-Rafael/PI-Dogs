bc7af9ff395d45a61d1819f907b54dbd0be774b3
Hans Garcia <hansvekoni@gmail.com>
2022-06-29 14:32:41 -0300

    modificacion del back para deploy

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import  {Provider} from 'react-redux';
 import store from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
