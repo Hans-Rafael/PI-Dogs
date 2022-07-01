499d4f2d7efc259572e2628b2dc98228fc8d6c53
Hans Garcia <hansvekoni@gmail.com>
2022-07-01 15:30:13 -0300

    front update

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
  dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001/api"; //
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"; //
 
 
 ReactDOM.render(
