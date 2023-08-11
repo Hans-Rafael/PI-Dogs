b4c5e699805d0c6805c72ace1d3cee118720c6ca
Hans <hansvekoni@gmail.com>
2023-08-11 10:35:15 -0300

    update test

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ conn.sync({ alter: true }).then(() => {
   });
 }); */
 //force: false to alter: true
-conn.sync({ alter: true }).then(() => {
+conn.sync({ alter: false }).then(() => {
   server.listen(port, () => {
     console.log('listening at',port); // eslint-disable-line no-console
   });
