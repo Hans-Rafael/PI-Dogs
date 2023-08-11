7ec77935a39c8b66f9907151b12bdec1407ab11c
Hans <hansvekoni@gmail.com>
2023-08-11 10:43:53 -0300

    upsade sync

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ conn.sync({ alter: true }).then(() => {
   });
 }); */
 //force: false to alter: true
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(port, () => {
     console.log('listening at',port); // eslint-disable-line no-console
   });
