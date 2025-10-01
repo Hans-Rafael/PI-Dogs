d55a3bebb047e05d1b88dca89338490f67e8900d
hansvekoni <hansvekoni@gmail.com>
2025-10-01 20:19:57 +0000

    fix redux

--- client/src/redux/reducerMain/index.js
+++ client/src/redux/reducerMain/index.js
@@ -1,5 +1,3 @@
-
-//import { GET_DOGS } from './../actions/index';
 import {
     GET_DOGS, CLEAR_PAGE, GET_BY_NAME, GET_TEMPERAMENT, FILTER_BY_CREATED, FILTER_BY_TEMPER,
     ORDER, POST, GET_DOGS_DETAIL
@@ -40,56 +38,38 @@ export default function reducer(state = initialState, action) {
 
             }
         case FILTER_BY_CREATED:
-            //const f =state.allDogs.filter(e => e.createdInDB)
-            const createdFilter = action.payload === 'DB'? state.allDogs.filter(e => e.createdInDB) : state.allDogs.filter(e => !e.createdInDB)   
+            const createdFilter = action.payload === 'DB' ? state.allDogs.filter(e => e.createdInDB) : state.allDogs.filter(e => !e.createdInDB);
             return {
                 ...state,
-                dogs: action.payload === 'All' ? state.allDogs : (createdFilter)? createdFilter : "db is empty"
+                dogs: action.payload === 'All' ? state.allDogs : createdFilter || []
             }
         case ORDER:
-            let sort
+            let sort;
+            const dogsToSort = [...state.dogs]; // Create a copy to avoid state mutation
             if (action.payload === 'Asc') {
-                sort = state.dogs.sort(function (a, b) {
-                    if (a.name > b.name) {
-                        return 1;
-                    }
-                    if (b.name > a.name) {
-                        return -1;
-                    }
-                    return 0;
-                })
-            } if (action.payload === 'Desc') {
-                sort = state.dogs.sort(function (a, b) {
-                    if (a.name > b.name) {
-                        return -1;
-                    }
-                    if (b.name > a.name) {
-                        return 1;
-                    }
+                sort = dogsToSort.sort(function (a, b) {
+                    if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+                    if (b.name.toLowerCase() > a.name.toLowerCase()) return -1;
                     return 0;
                 });
-            }
-            if (action.payload === 'Inc') {
-                sort = state.dogs.sort(function (a, b) {
-                    if (parseInt(a.weight.substr(0, 2)) > parseInt(b.weight.substr(0, 2))) {
-                        return 1;
-                    }
-                    if (parseInt(b.weight.substr(0, 2)) > parseInt(a.weight.substr(0, 2))) {
-                        return -1;
-                    }
+            } else if (action.payload === 'Desc') {
+                sort = dogsToSort.sort(function (a, b) {
+                    if (a.name.toLowerCase() > b.name.toLowerCase()) return -1;
+                    if (b.name.toLowerCase() > a.name.toLowerCase()) return 1;
                     return 0;
-                })
-            }
-            if (action.payload === 'Dec') {
-                sort = state.dogs.sort(function (a, b) {
-                    if (parseInt(a.weight.substr(0, 2)) > parseInt(b.weight.substr(0, 2))) {
-                        return -1;
-                    }
-                    if (parseInt(b.weight.substr(0, 2)) > parseInt(a.weight.substr(0, 2))) {
-                        return 1;
-                    }
-                    return 0;
-                })
+                });
+            } else if (action.payload === 'Inc') {
+                sort = dogsToSort.sort(function (a, b) {
+                    const weightA = parseInt(a.weight.split(' - ')[0]);
+                    const weightB = parseInt(b.weight.split(' - ')[0]);
+                    return weightA - weightB;
+                });
+            } else if (action.payload === 'Dec') {
+                sort = dogsToSort.sort(function (a, b) {
+                    const weightA = parseInt(a.weight.split(' - ')[0]);
+                    const weightB = parseInt(b.weight.split(' - ')[0]);
+                    return weightB - weightA;
+                });
             }
             return {
                 ...state,
